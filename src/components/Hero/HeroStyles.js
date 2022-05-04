0efa7d8b4a573806b114a2b685c065fa92288830
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-04 21:45:12 +0530

    added a herobutton ...might use or remove later

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -17,3 +17,16 @@ export const LeftSection = styled.div`
     margin: 0 auto;
   }
 `;
+
+
+export const Button = styled.button`
+  padding: 12px 16px;
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(42,205,82,1) 0%, rgba(0,212,255,1) 100%);
+  color: white;
+  font-size: 22px;
+  font-weight: bold;
+  border-radius: 10px;
+  border: 0;
+  cursor: pointer;
+`;
