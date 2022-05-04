86c8ce85bb5f455007e74e15d513d40e9ff59b59
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-04 21:46:16 +0530

    added section title text and cta button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,24 @@ import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
   <div>
-    Hero
+    <Section row nopadding>
+      <LeftSection>
+
+        <SectionTitle main>
+          Hey, I am Lakshit <br />Chiranjiv Sagar
+        </SectionTitle>
+
+        <SectionText>
+          Full Stack Web Developer and Blockchain Developer.<br />
+          I prefer JS stacks like MERN & MEVN. <br />
+          Feels good that you visited my Portfolio!!
+        </SectionText>
+
+        <Button onClick={()=>window.location='https://github.com/Lakshit-Chiranjiv'} wd={300}>
+          Check out my Projects
+        </Button>
+      </LeftSection>
+    </Section>
   </div>
 );
 
