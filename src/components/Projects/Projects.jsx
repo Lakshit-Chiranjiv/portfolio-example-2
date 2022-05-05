1f75544a4bd917a1b92a4cfd31bb5acd04189396
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-05 18:00:07 +0530

    Project section code done!!

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,9 +5,38 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id='projects'>
+    <SectionDivider/>
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {
+        projects.map(({id,title,image,description,source,tags,visit})=>(
+          <BlogCard key={id}>
+            <Img src={image}/>
+            <TitleContent>
+              <HeaderThree title>{title}</HeaderThree>
+              <Hr/>
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <TitleContent style={{marginTop: '6px'}}>Tech Stack</TitleContent>
+              <TagList>
+                {
+                  tags.map((tag,i) => (
+                    <Tag key={i}>{tag}</Tag>
+                  ))
+                }
+              </TagList>
+              <UtilityList>
+                <ExternalLinks href={visit} colorVal='#007a27'>Code</ExternalLinks>
+                <ExternalLinks href={source}>Demo</ExternalLinks>
+              </UtilityList>
+            </div>
+          </BlogCard>
+        ))
+      }
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
