79b3b0e83cffc1b5dd706e4df29829be99bdce88
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-07 17:31:25 +0530

    basic tech skills accordion ui done

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { FaEthereum } from 'react-icons/fa';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle,TechGroup,TechGroupToggleButton,TechGroupTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
   <div>
-    Technologies
+    <TechGroup>
+      <TechGroupTitle>Front-End Development <DiReact size='2rem' style={{ lineHeight: '20px' }}/></TechGroupTitle>
+      <TechGroupToggleButton>Open</TechGroupToggleButton>
+    </TechGroup>
+    <TechGroup>
+      <TechGroupTitle>Back-End Development <DiFirebase size='2rem' style={{ lineHeight: '20px' }}/></TechGroupTitle>
+      <TechGroupToggleButton>Open</TechGroupToggleButton>
+    </TechGroup>
+    <TechGroup>
+      <TechGroupTitle>Blockchain Development <FaEthereum size='2rem' style={{ lineHeight: '20px' }}/></TechGroupTitle>
+      <TechGroupToggleButton>Open</TechGroupToggleButton>
+    </TechGroup>
   </div>
 );
 
